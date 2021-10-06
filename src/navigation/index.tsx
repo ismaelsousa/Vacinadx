3b849fb0dbfbb404a643bba8a6dbc4105182561a
ismaelsousa <ismaelsousa1998@outlook.com>
2021-10-06 20:46:43 -0300

    change name home component

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from '~/screen/home';
+import Home from '~/screen/Home';
 import Profile from '~/screen/Profile';
 
 const Stack = createNativeStackNavigator();
