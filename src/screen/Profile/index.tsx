674055fe1cd17e19d5ddcc41e5a9cd68940ed5a4
ismaelsousa <ismaelsousa1998@outlook.com>
2021-10-06 19:54:40 -0300

    Moving between screens

--- src/screen/Profile/index.tsx
+++ src/screen/Profile/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import Text from '~/components/Text';
 
 import {Container} from './styles';
 
-const Profile: React.FC = () => {
+const Profile = ({navigation}) => {
+  const handleNavigation = () => navigation.push('profile');
+  const handleNavigationToTop = () => navigation.popToTop();
   return (
     <Container>
-      <Text>Profile</Text>
+      <TouchableOpacity onPress={handleNavigation}>
+        <Text>Profile</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleNavigationToTop}>
+        <Text>Pop to top</Text>
+      </TouchableOpacity>
     </Container>
   );
 };
