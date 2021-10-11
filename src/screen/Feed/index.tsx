74e13ef733bd462cbf8b6984a7e1a626d72eba93
ismaelsousa <ismaelsousa1998@outlook.com>
2021-10-11 07:45:19 -0300

    Navigate nested navigator

--- src/screen/Feed/index.tsx
+++ src/screen/Feed/index.tsx
@@ -7,9 +7,15 @@ const Feed: React.FC = ({navigation}) => {
   const handlePress = () => {
     navigation.navigate('stories');
   };
+  const handlePressToPost = () => {
+    navigation.navigate('profile', {
+      screen: 'post',
+    });
+  };
   return (
     <View style={{flex: 1, backgroundColor: '#ddd'}}>
       <Button title="Go to Stories" onPress={handlePress} />
+      <Button title="Go to posts" onPress={handlePressToPost} />
     </View>
   );
 };
