5b0ce42887ca5eaf63c27d4e94ee686e57ab2791
ismaelsousa <ismaelsousa1998@outlook.com>
2021-11-15 10:13:23 -0300

    Add miragejs and axios

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+if (__DEV__) {
+  require('./server');
+}
+
 AppRegistry.registerComponent(appName, () => App);
