d4b0cb62ca35488e78e4945d505ca4160cb98e86
ismaelsousa <ismaelsousa1998@outlook.com>
2021-11-15 10:18:16 -0300

    Revert "Add miragejs and axios"
    This reverts commit 5b0ce42887ca5eaf63c27d4e94ee686e57ab2791.

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-if (__DEV__) {
-  require('./server');
-}
-
 AppRegistry.registerComponent(appName, () => App);
