5f7848f30839c6282be632ec54f08663a407ec09
ismaelsousa <ismaelsousa1998@outlook.com>
2022-03-04 09:35:58 -0300

    Create profile screen

--- src/screen/SignedIn/Profile/index.tsx
+++ src/screen/SignedIn/Profile/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import {View} from 'react-native';
-
-// import {Container} from './styles'
+import {ScrollView, StatusBar, View} from 'react-native';
+import {useTheme} from 'styled-components';
+import Separator from '~/components/Separator';
+import Shadow from '~/components/Shadow';
+import Text from '~/components/Text';
+import useAuth from '~/hooks/useAuth';
+import Option from './localComponent/Option';
+import {Avatar, Container, Row, CardOption, Divider} from './styles';
 
 const Profile: React.FC = () => {
-  return <View />;
+  const {spacing} = useTheme();
+  const {user} = useAuth();
+  return (
+    <Container>
+      <ScrollView>
+        <StatusBar barStyle={'dark-content'} />
+        <Text typography="h5">Perfil</Text>
+        <Separator height={spacing.lg} />
+        <Row>
+          <Avatar source={{uri: user?.avatar}} />
+          <Separator width={spacing.sm} />
+          <View>
+            <Text typography="h7">{user?.firstName}</Text>
+            <Text typography="h7">{user?.lastName}</Text>
+          </View>
+        </Row>
+        <Separator height={spacing.lg} />
+        <Shadow>
+          <CardOption>
+            <Option title="Informações pessoais" icon="profile" />
+            <Divider />
+            <Option title="Suporte" icon="notification" />
+            <Divider />
+            <Option title="Sair" icon="logout" />
+          </CardOption>
+        </Shadow>
+      </ScrollView>
+    </Container>
+  );
 };
 
 export default Profile;
