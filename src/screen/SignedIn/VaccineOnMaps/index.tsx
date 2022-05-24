4e5bc06e03e1294497484b6c2ecae77598ae9ec5
ismaelsousa <ismaelsousa1998@outlook.com>
2022-05-24 20:57:27 -0300

    initial header maps

--- src/screen/SignedIn/VaccineOnMaps/index.tsx
+++ src/screen/SignedIn/VaccineOnMaps/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {useTheme} from 'styled-components';
+import Icon from '~/components/Icon';
+import Input from '~/components/Input';
+import Separator from '~/components/Separator';
+import {Header, HeaderContent, HeaderContentRow, InputRow} from './styles';
 
 // import {Container} from './styles'
 
 const VaccineOnMaps: React.FC = () => {
+  const {colors, spacing} = useTheme();
   return (
     <View style={styles.container}>
+      <Header>
+        <HeaderContent>
+          <HeaderContentRow>
+            <Icon icon="back" size={15} activeColor={colors.background.main} />
+            <Separator width={spacing.lg} />
+            <InputRow>{/* <Input /> */}</InputRow>
+          </HeaderContentRow>
+          <Separator height={spacing.lg} />
+        </HeaderContent>
+      </Header>
       <MapView
         provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : undefined} // remove if not using Google Maps
         style={styles.map}
