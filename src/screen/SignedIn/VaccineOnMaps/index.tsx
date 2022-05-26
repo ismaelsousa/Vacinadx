fea39381cefb412267ef28a5c40986ce9931858f
ismaelsousa <ismaelsousa1998@outlook.com>
2022-05-26 19:36:54 -0300

    Create header maps

--- src/screen/SignedIn/VaccineOnMaps/index.tsx
+++ src/screen/SignedIn/VaccineOnMaps/index.tsx
@@ -1,11 +1,41 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import {Platform, Pressable, StyleSheet, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-// import {Container} from './styles'
+import {useTheme} from 'styled-components';
+import Icon from '~/components/Icon';
+import Input from '~/components/Input';
+import Separator from '~/components/Separator';
+import {Header, HeaderContent, HeaderContentRow, InputRow} from './styles';
 
 const VaccineOnMaps: React.FC = () => {
+  const {goBack} = useNavigation();
+  const {colors, spacing} = useTheme();
   return (
     <View style={styles.container}>
+      <Header>
+        <HeaderContent>
+          <HeaderContentRow>
+            <Pressable onPress={goBack}>
+              <Icon
+                icon="back"
+                size={15}
+                activeColor={colors.background.main}
+              />
+            </Pressable>
+            <Separator width={spacing.lg} />
+            <InputRow>
+              <Input
+                icon="search"
+                color="primary"
+                placeholder="Busque por bairro"
+                iconPosition="left"
+              />
+            </InputRow>
+          </HeaderContentRow>
+          <Separator height={spacing.lg} />
+        </HeaderContent>
+      </Header>
       <MapView
         provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : undefined} // remove if not using Google Maps
         style={styles.map}
