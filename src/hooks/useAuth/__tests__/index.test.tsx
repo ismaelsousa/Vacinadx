5f3c2d4c9b90fe3d9ce9c484a69a417d89a1b5fe
ismaelsousa <ismaelsousa1998@outlook.com>
2022-06-05 13:06:59 -0300

    fix: name of test useAuth

--- src/hooks/useAuth/__tests__/index.test.tsx
+++ src/hooks/useAuth/__tests__/index.test.tsx
@@ -10,27 +10,29 @@ import {userMock} from '~/constants/mocks/user.mock';
 
 const apiMock = new MockAdapter(api);
 
-test('should increment counter', async () => {
-  apiMock
-    .onGet(`/users?email=${userMock.email}&password=${userMock.password}`)
-    .reply(200, [userMock]);
+describe('useAuth hook', () => {
+  test('should signIn with email and password', async () => {
+    apiMock
+      .onGet(`/users?email=${userMock.email}&password=${userMock.password}`)
+      .reply(200, [userMock]);
 
-  const {result, waitFor} = renderHook(() => useAuth(), {
-    wrapper: ({children}) => {
-      return <AuthProvider>{children}</AuthProvider>;
-    },
-  });
-
-  await waitFor(() => result.current?.loading === false);
-
-  await act(async () => {
-    await result.current.signIn({
-      email: userMock.email,
-      password: userMock.password,
+    const {result, waitFor} = renderHook(() => useAuth(), {
+      wrapper: ({children}) => {
+        return <AuthProvider>{children}</AuthProvider>;
+      },
     });
+
+    await waitFor(() => result.current?.loading === false);
+
+    await act(async () => {
+      await result.current.signIn({
+        email: userMock.email,
+        password: userMock.password,
+      });
+    });
+
+    expect(result.current.user?.email).toBe(userMock.email);
+
+    expect(true).toBeTruthy();
   });
-
-  expect(result.current.user?.email).toBe(userMock.email);
-
-  expect(true).toBeTruthy();
 });
