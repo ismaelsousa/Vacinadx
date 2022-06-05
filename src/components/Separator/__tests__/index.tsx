e9a57201f0bed83592f10a3d44903fb21e41cb1f
ismaelsousa <ismaelsousa1998@outlook.com>
2022-06-05 08:42:39 -0300

    feat: snapshot separator

---
+++ src/components/Separator/__tests__/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Separator from '..';
+
+describe('Separator', () => {
+  it('should render without props', () => {
+    const tree = renderer.create(<Separator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render with height prop', () => {
+    const tree = renderer.create(<Separator height={15} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render with width prop', () => {
+    const tree = renderer.create(<Separator width={15} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render with width and height props', () => {
+    const tree = renderer.create(<Separator width={15} height={15} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
