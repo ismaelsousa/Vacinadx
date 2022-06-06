971f624cefeb128827e41d1016a12402b6073cd3
ismaelsousa <ismaelsousa1998@outlook.com>
2022-06-06 08:55:52 -0300

    Add rehydrate test

--- src/hooks/useAuth/__tests__/index.test.tsx
+++ src/hooks/useAuth/__tests__/index.test.tsx
@@ -11,7 +11,7 @@ import {userMock} from '~/constants/mocks/user.mock';
 const apiMock = new MockAdapter(api);
 
 describe('useAuth hook', () => {
-  test('should signIn with email and password', async () => {
+  it('should signIn with email and password', async () => {
     apiMock
       .onGet(`/users?email=${userMock.email}&password=${userMock.password}`)
       .reply(200, [userMock]);
@@ -30,6 +30,27 @@ describe('useAuth hook', () => {
         password: userMock.password,
       });
     });
+
+    expect(result.current.user?.email).toBe(userMock.email);
+
+    expect(true).toBeTruthy();
+  });
+
+  it('should rehydrate with user', async () => {
+    jest.mock('@react-native-async-storage/async-storage', () => {
+      const user = require('~/constants/mocks/user.mock');
+      return {
+        getItem: jest.fn(() => Promise.resolve(JSON.stringify(user))),
+      };
+    });
+
+    const {result, waitFor} = renderHook(() => useAuth(), {
+      wrapper: ({children}) => {
+        return <AuthProvider>{children}</AuthProvider>;
+      },
+    });
+
+    await waitFor(() => result.current?.loading === false);
 
     expect(result.current.user?.email).toBe(userMock.email);
 
